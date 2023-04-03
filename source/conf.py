a20db732a82a18a07c237b44699e1e64e70f737e
JunFN888 <junfengwang8888@163.com>
2023-04-03 17:58:09 +0800

    AUMO_1.3

--- source/conf.py
+++ source/conf.py
@@ -45,4 +45,10 @@ html_static_path = ['_static']
 #html_theme = 'sphinx_pdj_theme'
 #html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
 
-html_theme = 'nature'
+#html_theme = 'nature'
+
+#html_theme = 'topos-theme'
+
+#html_theme = 'yummy_sphinx_theme'
+
+html_theme = 'renku'
