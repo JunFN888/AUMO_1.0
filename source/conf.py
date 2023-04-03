9f25f6956a5b49ac0b8d631be5c21737a28f4958
JunFN888 <junfengwang8888@163.com>
2023-04-03 15:52:55 +0800

    AUMO_1.1

--- source/conf.py
+++ source/conf.py
@@ -10,6 +10,7 @@ project = '16 路车载摄像头 GMSL 采集卡操作手册'
 copyright = '2023, AUMO       https://www.aumo.cn/'
 author = 'JunFN'
 release = '1.0'
+#import sphinx-groundwork-theme
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
@@ -36,5 +37,10 @@ language = 'zh_CN'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 #html_theme = 'alabaster'
-html_theme = 'groundwork'
 html_static_path = ['_static']
+#html_theme = 'groundwork'
+#html_theme_path = [sphinx-groundwork-theme.get_html_theme_path()]
+
+import sphinx_pdj_theme
+html_theme = 'sphinx_pdj_theme'
+html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
